5abab1f4355abd362958db33e5cad047669a0bc2
Petar Scepanovic <hiborija@gmail.com>
2019-07-15 15:17:07 +0200

    set up database, added starting records, name and budget. Wrote endpoints for fetching all the accounts from db and adding a new account. Added middleware for error handling

--- server.js
+++ server.js
@@ -6,4 +6,44 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res, next) => {
+  res.json('success!');
+});
+
+function getAllAccounts() {
+  return db('accounts');
+}
+
+function getAccountById(id) {
+  return db('accounts').where({ id });
+}
+
+function createNewAccount({ name, budget }) {
+  return db('accounts').insert({ name, budget });
+}
+
+server.get('/accounts', async (req, res) => {
+  const accounts = await getAllAccounts();
+  res.json(accounts);
+});
+
+server.post('/accounts', async (req, res, next) => {
+  try {
+    const arrayOfIds = await createNewAccount(req.body);
+    const arrayOfAccounts = await getAccountById(arrayOfIds[0]);
+    res.status(201).json(arrayOfAccounts[0]);
+  } catch (error) {
+    next(new Error("Couldn't create account"));
+  }
+});
+
+
+server.use(function errorHandler(err, req, res, next) {
+  console.error('ERROR:', err);
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  });
+});
+
 module.exports = server;
